c7ff22d7d813e0b76a9f1bac118a6e9f17324a7c
austin minnon and kyle derrick <kderrick1988@gmail.com>
2016-05-02 14:31:00 -0700

    add NewPostActivity and functionality to create new Post on firebase

--- app/src/main/java/com/epicodus/discussionforum/Constants.java
+++ app/src/main/java/com/epicodus/discussionforum/Constants.java
@@ -2,5 +2,6 @@ package com.epicodus.discussionforum;
 
 public class Constants {
     public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+    public static final String FIREBASE_URL_POSTS = FIREBASE_URL + "/POSTS";
 
 }
--- app/src/main/java/com/epicodus/discussionforum/ui/NewPostActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/ui/NewPostActivity.java
@@ -1,15 +1,51 @@
 package com.epicodus.discussionforum.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.epicodus.discussionforum.Constants;
 import com.epicodus.discussionforum.R;
+import com.epicodus.discussionforum.models.ForumPost;
+import com.firebase.client.Firebase;
+import com.firebase.client.ValueEventListener;
 
-public class NewPostActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewPostActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private String title;
+    private String author;
+    private String body;
+    @Bind(R.id.titleEditText) EditText mTitleEditText;
+    @Bind(R.id.authorEditText) EditText mAuthorEditText;
+    @Bind(R.id.bodyEditText) EditText mBodyEditText;
+    @Bind(R.id.submitButton) Button mSubmitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+        ButterKnife.bind(this);
+
+        mSubmitButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSubmitButton) {
+           ForumPost newForumPost = new ForumPost(mTitleEditText.getText().toString(), mAuthorEditText.getText().toString(), mBodyEditText.getText().toString());
+            Firebase ref = new Firebase(Constants.FIREBASE_URL_POSTS);
+            ref.push().setValue(newForumPost);
+            Intent backToMain = new Intent(NewPostActivity.this, MainActivity.class);
+            startActivity(backToMain);
+        }
     }
 }
